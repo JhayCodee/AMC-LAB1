1050f50fbc4afa8fad36054335d494dd44fe736a
JhayCodee <kluis0178@gmail.com>
2024-03-27 18:26:06 -0600

    reestructura

--- JS/calculator.js
+++
@@ -1,8 +0,0 @@
-export function calculatePerformance(data) {
-    // Aquí iría la lógica de cálculo usando los datos del formulario
-    // Por ejemplo, devolver un objeto con los resultados
-    return {
-        processor1: { /* Resultados del procesador 1 */ },
-        processor2: { /* Resultados del procesador 2 */ }
-    };
-}
